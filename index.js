c6d5aefdaa00d13a534d5c4338afa57fc60cabe1
Peter Coughlin <coughlin1966@gmail.com>
2017-11-16 17:02:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 function shout(string) {
   return string.toUpperCase()
 }
-shout('Hello!)
+shout('Hello')
+
 function whisper(string) {
   return string.toLowerCase()
 }
 whisper('Hello')
+
 function logShout(string) {
-  console.log(string)
+  console.log(string.toUpperCase)
 }
-logShout('Hello!')
+logShout('Hello')
