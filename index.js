66fd76aa3457e4bfbb021eba2aceaa8310a085eb
Peter Coughlin <coughlin1966@gmail.com>
2017-11-16 21:50:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,10 +28,12 @@ var mixedcase = 'Hi There'
 
 function sayHiToGrandma(string) {
   if (lowercase.toLowerCase() === lowercase) {return('I can\'t hear you!')}
-else if (uppercase.toUpperCase() === uppercase) {return('YES INDEED!')}
-else if (string === 'I love you, Grandma.'){return('I love you, too.')}
-else {return('I love you, too.')}
 }
+function sayHiToGrandma(string) {if (uppercase.toUpperCase() === uppercase) {return('YES INDEED!')}
+}
+function sayHiToGrandma(string) {if (string === 'I love you, Grandma.'){return('I love you, too.')}
+}
+
 sayHiToGrandma('HELLO')
 sayHiToGrandma('hello')
 sayHiToGrandma('I love you, Grandma.')
